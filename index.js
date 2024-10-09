b5eb740ccbb7ae3a5c0bc5c4da61bad8966e8500
wetshakat <ishakudyelshak@gmail.com>
2024-10-09 22:31:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// // learning
+
+// let name = 'Dyelshak ishaku wetshakat'
+// let age = 25
+// let nationality = "Nigeria"
+// let state = 'plateau'
+
+// let solution = name.concat("age" + " nationality" ) 
+
+// let sentence = "Valantine" +  " is'a" + " nigerian" 
+// console.log(sentence)
+// console.log(name.charAt(10))
+// console.log(name.length)
+//  let tally = name + age + nationality + state
+//  console.log(tally.length)
+//  console.log(tally.charAt(31))
+//  console.log(tally.slice(15,33))
+//  console.log(tally.slice(15,33))
+
+let character = "dyelshak"
+console.log(character)
+character = "Wetshakat"
+console.log(character)
+let secondCharacter;
+console.log(secondCharacter)
+secondCharacter = "emerson"
+console.log(secondCharacter)
+let score = 15
+console.log(15+5)
