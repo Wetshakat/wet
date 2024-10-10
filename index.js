36a4de26e96346f0bc6da534dc9e667ac728f463
wetshakat <ishakudyelshak@gmail.com>
2024-10-10 02:46:33 +0100

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // // learning
 
-// let name = 'Dyelshak ishaku wetshakat'
-// let age = 25
-// let nationality = "Nigeria"
-// let state = 'plateau'
+//  let name = 'Dyelshak ishaku wetshakat'
+// /let age = 25
+//  let nationality = "Nigeria"
+//  let state = 'plateau'
 
 // let solution = name.concat("age" + " nationality" ) 
 
@@ -17,13 +17,43 @@
 //  console.log(tally.slice(15,33))
 //  console.log(tally.slice(15,33))
 
-let character = "dyelshak"
-console.log(character)
-character = "Wetshakat"
-console.log(character)
-let secondCharacter;
-console.log(secondCharacter)
-secondCharacter = "emerson"
-console.log(secondCharacter)
-let score = 15
-console.log(15+5)
+//  let character = "dyelshak"
+//  console.log(character)
+//  character = "Wetshakat"
+// console.log(character)
+//  let secondCharacter;
+// console.log(secondCharacter)
+//  secondCharacter = "emerson"
+// console.log(secondCharacter)
+//  let score = 15
+// console.log(15+5)
+// // the number method permits you to perform all kind of arithmetics functions
+
+// let Boys = 20
+// let Girls = 35
+
+// let result = 20*35
+// console.log(result)
+
+// let a = (20)*2
+// let b = (35)*2 
+
+// let tally = a+b
+// console.log(tally)
+
+// // an array is a special variable that containes more than one value.
+
+// console.log(typeof(Boys))
+
+let schools = ["BSS", "CCG","GHS"]
+// console.log(schools);
+// schools[2] = 50;
+// console.log(schools[2])
+
+// schools[schools.length-1]
+// console.log(schools)
+
+let state = ["Plateau","Bauchi", "Jigawa", "Kano"];
+console.log(state);
+state[state.length -1]= "Gombe";
+console.log(state)
